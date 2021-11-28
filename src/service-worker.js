f3c1171d2f9366bf4a722918669860730a9db959
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-11-28 11:53:27 -0300

    Finalizada la implementación manual del service-worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -80,3 +80,27 @@ self.addEventListener('install', async (event) => {
     '/favicon.ico',
   ]);
 });
+
+self.addEventListener('fetch', (event) => {
+  const apiOfflineFallbacks = [
+    'http://localhost:4000/api/auth/renew',
+    'http://localhost:4000/api/events',
+  ];
+  if (!apiOfflineFallbacks.includes(event.request.url)) return;
+  const resp = fetch(event.request)
+    .then((response) => {
+      if (!response) {
+        return caches.match(event.request);
+      }
+      // Guardar en cache la respuesta
+      caches.open('cache-dynamic').then((cache) => {
+        cache.put(event.request, response);
+      });
+      return response.clone();
+    })
+    .catch((err) => {
+      console.log('offline response');
+      return caches.match(event.request);
+    });
+  event.respondWith(resp);
+});
