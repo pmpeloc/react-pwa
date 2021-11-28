9fc31d8131d5409220cfdb5dc244e4b35a2c85e4
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-11-28 14:54:40 -0300

    PWA finalizada

--- src/sw-template.js
+++ src/sw-template.js
@@ -1,35 +1,60 @@
+/* eslint-disable no-restricted-globals */
 /* eslint-disable no-undef */
-// eslint-disable-next-line no-undef
 importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js'
 );
 
-// eslint-disable-next-line
+workbox.loadModule('workbox-background-sync');
+
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 const { registerRoute } = workbox.routing;
-const { CacheFirst, NetworkFirst } = workbox.strategies;
+const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
 
-registerRoute(
-  new RegExp('http://localhost:4000/api/auth/renew'),
-  new NetworkFirst()
-);
+const cacheNetworkFirst = ['/api/auth/renew', '/api/events'];
+
+registerRoute(({ request, url }) => {
+  if (cacheNetworkFirst.includes(url.pathname)) return true;
+  return false;
+}, new NetworkFirst());
+
+const cacheFirstNetwork = [
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css',
+];
+
+registerRoute(({ request, url }) => {
+  if (cacheFirstNetwork.includes(url.href)) return true;
+  return false;
+}, new CacheFirst());
+
+// Posteos Offline
+
+const bgSyncPlugin = new BackgroundSyncPlugin('posteos-offline', {
+  maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
+});
 
 registerRoute(
   new RegExp('http://localhost:4000/api/events'),
-  new NetworkFirst()
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  'POST'
 );
 
 registerRoute(
-  new RegExp(
-    'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'
-  ),
-  new CacheFirst()
+  new RegExp('http://localhost:4000/api/events/'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  'PUT'
 );
 
 registerRoute(
-  new RegExp(
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'
-  ),
-  new CacheFirst()
+  new RegExp('http://localhost:4000/api/events/'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  'DELETE'
 );
