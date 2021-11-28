71c91dbff8571eafbf28d5aa64c9d8a687473aac
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-11-28 13:06:16 -0300

    Implementando Workbox para renew y events

---
+++ src/sw-template.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js'
+);
+
+// eslint-disable-next-line
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst, NetworkFirst } = workbox.strategies;
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/auth/renew'),
+  new NetworkFirst()
+);
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events'),
+  new NetworkFirst()
+);
+
+registerRoute(
+  new RegExp(
+    'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'
+  ),
+  new CacheFirst()
+);
+
+registerRoute(
+  new RegExp(
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'
+  ),
+  new CacheFirst()
+);
---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  globDirectory: 'build/',
+  globPatterns: ['**/*.{json,ico,html,png,txt,css,js}'],
+  // ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+  swDest: 'build/sw.js',
+  swSrc: 'src/sw-template.js',
+};
